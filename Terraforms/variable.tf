3a11324fe837ba6380d8c71e6e3e26bea0dc4ce5
shoaib <shoaib.bhorey@gmail.com>
2023-05-22 10:18:32 +0500

    workflow

--- Terraforms/variable.tf
+++ Terraforms/variable.tf
@@ -7,14 +7,6 @@
     description = "ECS task execution role name"
  }
 
-variable "app_image" {
-   default   = "80"
-   description  = "portexposed on the docker image"
-}
 
-variable fargate_cpu {
-   default = "1024"
-   description = "fargate instance CPU units to provision "
-}
  
  
